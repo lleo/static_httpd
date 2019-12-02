241649a56e9e0430b5d515f56b87ccac37660ed8
LLeo <lleoem@gmail.com>
2019-12-02 07:02:48 -0500

    added shell script for self signing certificates and tweaked main.go

--- main.go
+++ main.go
@@ -15,6 +15,8 @@ func main() {
 
 	fmt.Println(os.Args)
 
+	var lgr = log.New(os.Stdout, "Logger:", log.Lshortfile|log.Ldate|log.Ltime)
+
 	var err error
 
 	cwd, err := os.Getwd()
@@ -23,22 +25,49 @@ func main() {
 	}
 
 	var hostport string
-	flag.StringVar(&hostport, "hostport", "localhost:8080", "A string of the form \"{host}:{port}\"")
+	flag.StringVar(&hostport, "hostport", "localhost:8081", "A string of the form \"{host}:{port}\"")
 
-	var rootd string
-	flag.StringVar(&rootd, "rootdir", cwd, "The root directory of static files to be served.")
+	flag.Parse()
 
-	rootd, err = filepath.Abs(rootd)
+	var twistedDocs string = "/Users/seanegan/source/Twisted-19.10.0/docs/_build/html"
+
+	twistedRootStr, err := filepath.Abs(twistedDocs)
 	if err != nil {
 		log.Fatal(err)
 	}
-	flag.Parse()
+
+	var twistedRoot = http.Dir(twistedRootStr)
 
 	//Need to verify rootd is an existing directory
 
 	fmt.Printf("hostport=%s\n", hostport)
 	fmt.Printf("cwd=%s\n", cwd)
-	fmt.Printf("rootd=%s\n", rootd)
+	fmt.Printf("twistedRoot=%s\n", twistedRoot)
 
-	log.Fatal(http.ListenAndServe(hostport, http.FileServer(http.Dir(rootd))))
+	// create mux
+	var mux = http.NewServeMux()
+
+	// register /twisted/
+	var twistedHandler = http.StripPrefix("/twisted/", http.FileServer(twistedRoot))
+	mux.Handle("/twisted/", twistedHandler)
+
+	// build Server
+	var svr = &http.Server{
+		Addr:    hostport,
+		Handler: mux,
+		//TLSConfig: *tls.Config,
+		//ReadTimeout: time.Duration,
+		//ReadHeaderTimeout: time.Duration,
+		//WriteTimeout: time.Duration,
+		//IdleTimeout: time.Duration,
+		//MaxHeaderBytes: int,
+		//TLSNextProto: map[string]func(net.Conn, ConnState),
+		//ConnState: func(net.Conn, ConnState),
+		ErrorLog: lgr,
+		//BaseContext: func(net.Listener) context.Context,
+		//ConnContext: func(context.Context, net.Conn) context.Context,
+	}
+
+	lgr.Println("starting server...")
+	lgr.Fatal(svr.ListenAndServe())
 }
